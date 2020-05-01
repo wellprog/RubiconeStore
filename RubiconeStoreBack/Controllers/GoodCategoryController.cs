9a7b6c6683a2aada68df81e6ab8cdc4b5564cdfd
googleKrakenPlex <iambagger10@gmail.com>
2020-05-01 21:33:48 +0300

    10 - Добавлены GetAll методы для GoodCategoryController и GoodPropertyController

--- RubiconeStoreBack/Controllers/GoodCategoryController.cs
+++ RubiconeStoreBack/Controllers/GoodCategoryController.cs
@@ -5,7 +5,29 @@ using System.Threading.Tasks;
 
 namespace RubiconeStoreBack.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class GoodCategoryController
     {
+        private readonly ILogger<UserListController> _logger;
+        private readonly DbStore _store;
+        private readonly UserHelper _userHelper;
+
+        public GoodCategoryController(ILogger<UserListController> logger, DbStore store, UserHelper userHelper)
+        {
+            this._logger = logger;
+            this._store = store;
+            this._userHelper = userHelper;
+        }
+
+        [HttpGet]
+        public ResponceModel<IEnumerable<GoodCategory>> GetAll(string AuthKey)
+        {
+            var responce = _userHelper.IsUserAutorized<IEnumerable<GoodCategory>>(AuthKey);
+            if (responce != null) return responce;
+
+            var resp = _store.GoodCategories; //!
+            return new ResponceModel<IEnumerable<GoodCategory>> { content = resp };
+        }
     }
 }
--- RubiconeStoreBack/Controllers/GoodPropertyController.cs
+++ RubiconeStoreBack/Controllers/GoodPropertyController.cs
@@ -5,7 +5,29 @@ using System.Threading.Tasks;
 
 namespace RubiconeStoreBack.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class GoodPropertyController
     {
+        private readonly ILogger<UserListController> _logger;
+        private readonly DbStore _store;
+        private readonly UserHelper _userHelper;
+
+        public GoodPropertyController(ILogger<UserListController> logger, DbStore store, UserHelper userHelper)
+        {
+            this._logger = logger;
+            this._store = store;
+            this._userHelper = userHelper;
+        }
+
+        [HttpGet]
+        public ResponceModel<IEnumerable<GoodProperty>> GetAll(string AuthKey)
+        {
+            var responce = _userHelper.IsUserAutorized<IEnumerable<GoodProperty>>(AuthKey);
+            if (responce != null) return responce;
+
+            var resp = _store.GoodProperties; //!
+            return new ResponceModel<IEnumerable<GoodProperty>> { content = resp };
+        }
     }
 }
