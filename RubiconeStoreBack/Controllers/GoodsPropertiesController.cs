2635d42aa6fb7add2c073bf711bc687808756f96
Velor <VelorKhalilov@gmail.com>
2020-05-10 19:09:59 +0300

    New controller

---
+++ RubiconeStoreBack/Controllers/GoodsPropertiesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RubiconeStoreBack.DataAccess;
+using RubiconeStoreBack.Error;
+using RubiconeStoreBack.Helpers;
+
+using Shared.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RubiconeStoreBack.Controllers
+{
+    [ApiController]
+    public class GoodsPropertiesController : BaseController<GoodsPropertiesController>
+    {
+        public GoodsPropertiesController(ILogger<GoodsPropertiesController> logger, DbStore store, UserHelper userHelper) : base(logger, userHelper, store)
+        { }
+
+        [Route("[controller]/{AuthKey}/{ElementId}")]
+        [HttpGet]
+        public ResponceModel<GoodPropertiesModel> PropertiesForGood(string AuthKey, int ElementId)
+        {
+            var good = _store.Goods.Where(f => f.ID == ElementId).FirstOrDefault();
+            if (good == null)
+                return new ResponceModel<GoodPropertiesModel>().RecordNotFound();
+
+            var properties = _store.GoodProperties.Where(f => f.GoodCategoryID == good.GoodCategoryID);
+            var values = _store.GoodPropertyValues.Where(f => f.GoodID == good.ID);
+
+            return new ResponceModel<GoodPropertiesModel>()
+            {
+                content = new GoodPropertiesModel()
+                {
+                    Good = good,
+                    GoodProperties = properties,
+                    GoodPropertyValues = values
+                }
+            };
+        }
+
+        public ResponceModel<GoodPropertyValue> SetPropertyForGood(RequestModel<GoodPropertyValue> request)
+        {
+            var good = _store.Goods.Where(f => f.ID == request.Content.GoodID).FirstOrDefault();
+            if (good == null)
+                return new ResponceModel<GoodPropertyValue>().RecordNotFound();
+
+
+        }
+    }
+}
---
+++ Shared/Model/GoodPropertiesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Model
+{
+    public class GoodPropertiesModel
+    {
+        public Good Good { get; set; }
+        public IEnumerable<GoodProperty> GoodProperties { get; set; }
+        public IEnumerable<GoodPropertyValue> GoodPropertyValues { get; set; }
+    }
+}
