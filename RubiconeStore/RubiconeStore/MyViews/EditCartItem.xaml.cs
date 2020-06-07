4f703e8b5732258c7cf6f75e2ee03cd6fa2fff35
googleplexplex <iambagger10@gmail.com>
2020-06-07 18:43:45 +0300

    Добавил viewModel для товаров в корзине

---
+++ RubiconeStore/RubiconeStore/MyViews/EditCartItem.xaml.cs
@@ -0,0 +1,36 @@
+﻿using RubiconeStore.MyViewModels;
+
+using Shared.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RubiconeStore.MyViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditCartItem : ContentPage
+    {
+        private readonly EditCartItemViewModel model;
+        public EditCartItem() : this(new CartItemModel()) { }
+
+        public EditCartItem(CartItemModel cartItem)
+        {
+            InitializeComponent();
+            model = new EditCartItemViewModel(cartItem, new HttpClient(), this);
+            BindingContext = model;
+        }
+
+        protected async override void OnAppearing()
+        {
+            await model.Appearing();
+            base.OnAppearing();
+        }
+    }
+}
