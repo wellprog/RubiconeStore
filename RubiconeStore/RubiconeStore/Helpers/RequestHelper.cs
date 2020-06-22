0d8e19e3bef5162e88c33f53f2c87c5d71fe83c3
googleplexplex <iambagger10@gmail.com>
2020-06-22 16:24:07 +0300

    Каждому методу в RequestUser добавлен аналог с постфиксом WithResponce, отличающийся от оригинала возвращением ResonceModel<T> вместо T

--- RubiconeStore/RubiconeStore/Helpers/RequestHelper.cs
+++ RubiconeStore/RubiconeStore/Helpers/RequestHelper.cs
@@ -24,7 +24,7 @@ namespace RubiconeStore.Helpers
             this.httpClient = new HttpClient();
         }
 
-        public async Task<T> Get<T>(string URL, Dictionary<string, object> queryParams = null)
+        public async Task<ResponceModel<T>> GetWithResponce<T>(string URL, Dictionary<string, object> queryParams = null)
         {
             string url = Url(URL, queryParams);
 
@@ -32,44 +32,58 @@ namespace RubiconeStore.Helpers
             return await GetResponce<T>(responce);
         }
 
-        public async Task<ResponceModel<T>> GetWithResponce<T>(string URL, Dictionary<string, object> queryParams = null)
+        public async Task<T> Get<T>(string URL, Dictionary<string, object> queryParams = null)
         {
-            string url = Url(URL, queryParams);
-
-            var responce = await httpClient.GetAsync(url);
-            return await GetAllResponce<T>(responce);
+            return (await GetWithResponce<T>(URL, queryParams)).content;
         }
 
-        public async Task<T> Delete<T>(string URL, Dictionary<string, object> queryParams = null)
+        public async Task<ResponceModel<T>> DeleteWithResponce<T>(string URL, Dictionary<string, object> queryParams = null)
         {
             string url = Url(URL, queryParams);
 
             var responce = await httpClient.DeleteAsync(url);
             return await GetResponce<T>(responce);
         }
+        public async Task<T> Delete<T>(string URL, Dictionary<string, object> queryParams = null)
+        {
+            return (await DeleteWithResponce<T>(URL, queryParams)).content;
+        }
 
-        public async Task<T> Post<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
+        public async Task<ResponceModel<T>> PostWithResponce<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
         {
             string url = Url(URL, queryParams);
             var responce = await httpClient.PostAsync(url, new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json"));
             return await GetResponce<T>(responce);
         }
+        public async Task<T> Post<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
+        {
+            return (await PostWithResponce<T, Req>(URL, request, queryParams)).content;
+        }
 
-        public async Task<T> Put<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
+        public async Task<ResponceModel<T>> PutWithResponce<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
         {
             string url = Url(URL, queryParams);
             var responce = await httpClient.PutAsync(url, new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json"));
             return await GetResponce<T>(responce);
         }
+        public async Task<T> Put<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
+        {
+            return (await PutWithResponce<T, Req>(URL, request, queryParams)).content;
+        }
 
-        public async Task<T> Patch<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
+        public async Task<ResponceModel<T>> PatchWithResponce<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
         {
             string url = Url(URL, queryParams);
             var responce = await httpClient.SendAsync(new HttpRequestMessage(new HttpMethod("PATCH"), url) { Content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json") });
             return await GetResponce<T>(responce);
         }
+        public async Task<T> Patch<T, Req>(string URL, Req request, Dictionary<string, object> queryParams = null)
+        {
+            return (await PatchWithResponce<T, Req>(URL, request, queryParams)).content;
+        }
 
-        private static async Task<ResponceModel<T>> GetAllResponce<T>(HttpResponseMessage responce)
+
+        private static async Task<ResponceModel<T>> GetResponce<T>(HttpResponseMessage responce)
         {
             if (responce.StatusCode != System.Net.HttpStatusCode.OK)
             {
@@ -80,10 +94,6 @@ namespace RubiconeStore.Helpers
             ResponceModel<T> model = JsonConvert.DeserializeObject<ResponceModel<T>>(jsonString);
 
             return model;
-        }
-        private static async Task<T> GetResponce<T>(HttpResponseMessage responce)
-        {
-            return (await GetAllResponce<T>(responce)).content;
         }
 
 
